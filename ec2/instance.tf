89ff8eaefb8edd969615651b753c72afadeedc78
root <root@localhost.localdomain>
2017-05-05 20:35:54 +0000

    removed creds from tf files and commiting

--- ec2/instance.tf
+++ ec2/instance.tf
@@ -1,6 +1,4 @@
 provider "aws" {
-  access_key = "AKIAIJHNNLD5V5RYIIRQ"
-  secret_key = "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"
   region     = "ap-south-1"
 }
 
--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,14 +1,7 @@
 provider "aws" {
-	access_key = "AKIAIJHNNLD5V5RYIIRQ"
-	secret_key = "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"
-	region     = "ap-south-1"
+  region     = "ap-south-1"
 }
 
 resource "aws_vpc" "main" {
-	
-	cidr_block = "10.0.0.0/16"
-
-	tags {
-	Name = "main"
-	}
+  cidr_block = "10.0.0.0/16"
 }
