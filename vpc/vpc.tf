0258ce26ae64823ac0088f5360c869cc83e8af0f
root <root@localhost.localdomain>
2017-05-06 13:14:28 +0000

    added commented lines for description in vpc.tf file

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,7 +1,9 @@
+#define the cloud provider
 provider "aws" {
   region     = "ap-south-1"
 }
 
+#this block creates vpc with the mentioned cidr and tag details
 resource "aws_vpc" "main" {
   
   cidr_block = "10.0.0.0/16"
@@ -13,6 +15,7 @@ resource "aws_vpc" "main" {
 
 }
 
-  output "vpc_id" {
-    value = "${aws_vpc.main.id}"
-  }
+#displays vpc ID on console
+output "vpc_id" {
+  value = "${aws_vpc.main.id}"
+}
