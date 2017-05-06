4404f1699fa499c7f16a0b4b690f03a40bf8883d
root <root@localhost.localdomain>
2017-05-06 13:22:08 +0000

    added public subnet resource

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -19,3 +19,14 @@ resource "aws_vpc" "main" {
 output "vpc_id" {
   value = "${aws_vpc.main.id}"
 }
+
+#creates public subnet
+resource "aws_subnet" "public" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags {
+    Name = "Public_Subnet"
+  }
+}
