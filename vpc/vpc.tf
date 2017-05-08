fa9d687a0f95e11a910c2c4ecd3e8dc482401720
root <root@localhost.localdomain>
2017-05-08 11:20:58 -0700

    added route table association resource

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -6,7 +6,7 @@ provider "aws" {
 #this block creates vpc with the mentioned cidr and tag details
 resource "aws_vpc" "main" {
   
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "192.168.0.0/16"
   instance_tenancy = "default"
 
   tags {
@@ -20,10 +20,10 @@ output "vpc_id" {
   value = "${aws_vpc.main.id}"
 }
 
-#creates public subnet
+#creates public subnet and default route table
 resource "aws_subnet" "public" {
   vpc_id     = "${aws_vpc.main.id}"
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "192.168.0.0/16"
   availability_zone = "ap-south-1a"
 
   tags {
@@ -45,8 +45,8 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-#adds igw to route table
-resource "aws_route_table" "r" {
+#creates new route table and adds igw to route table
+resource "aws_route_table" "public" {
   vpc_id = "${aws_vpc.main.id}"
 
   route {
@@ -57,4 +57,10 @@ resource "aws_route_table" "r" {
   tags {
     Name = "public_route"
   }
+}
+
+#route table association
+resource "aws_route_table_association" "public" {
+    subnet_id = "${aws_subnet.public.id}"
+    route_table_id = "${aws_route_table.public.id}"
 }
